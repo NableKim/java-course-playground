be65a44db7d5efe4afa8571eea6bb55ad6a643b5
nablekim94 <nablekim94@gmail.com>
2021-11-22 18:11:13 +0900

    함수를 사용하여 구구단 계산 및 출력 로직 분리

--- MultiplicationTable/src/firstSource.java
+++ MultiplicationTable/src/firstSource.java
@@ -1,19 +1,24 @@
 import java.util.Scanner;
 
 public class firstSource {
+	
+	public static int[] calculate(int number) {
+		int[] result = new int[10];
+		for(int i=1; i<=9; i++)
+			result[i] = i * number;
+		return result;
+	}
+	
+	public static void print(int number, int[] result) {
+		System.out.println("==== "+number+"단 ====");
+		for(int i=1; i<=9; i++)
+			System.out.println(number+ " X " + i + " = " + result[i]);
+	}
+	
 	public static void main(String[] args) {
 		for(int number =2; number<=9; number++) {
-			// 배열 생성
-			int[] result = new int[10];
-			
-			// 구구단 계산값을 배열에 저장
-			for(int j=1; j<=9; j++)
-				result[j] = number * j;
-			
-			// 구구단 결과 출력
-			System.out.println("==== "+number+"단 ====");
-			for(int j=1; j<=9; j++)
-				System.out.println(number+ " X " + j + " = " + result[j]);
+			int[] result = calculate(number);
+			print(number, result);
 		}
 	}
 }
