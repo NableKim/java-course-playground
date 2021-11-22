4fed27d3375055db3651c219a593f584d9832d12
nablekim94 <nablekim94@gmail.com>
2021-11-22 17:58:51 +0900

    구구단 결과값을 배열에 저장하여 출력하도록 구문 수정

--- MultiplicationTable/src/firstSource.java
+++ MultiplicationTable/src/firstSource.java
@@ -9,9 +9,19 @@ public class firstSource {
 		
 		// 2~9 범위의 숫자를 올바르게 입력받았는가
 		if(number>=2 && number<=9) {
+			
+			// 배열 생성
+			int[] result = new int[10];
+			
+			// 구구단 계산값을 배열에 저장
+			for(int j=1; j<=9; j++)
+				result[j] = number * j;
+			
+			// 구구단 결과 출력
 			System.out.println("==== "+number+"단 ====");
 			for(int j=1; j<=9; j++)
-				System.out.println(number+" X "+j+" = " + number*j);
+				System.out.println(number+ " X " + j + " = " + result[j]);
+			
 		} else {
 			System.out.println("2~9 범위의 숫자를 입력해주세요!!!");
 		}
