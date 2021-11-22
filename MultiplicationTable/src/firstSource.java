fe3821328f4cff72e2452ae29332bff99a39ff73
nablekim94 <nablekim94@gmail.com>
2021-11-22 18:00:34 +0900

    반복문을 사용해서 2~9단 전부 출력하는 로직 구현

--- MultiplicationTable/src/firstSource.java
+++ MultiplicationTable/src/firstSource.java
@@ -2,14 +2,7 @@ import java.util.Scanner;
 
 public class firstSource {
 	public static void main(String[] args) {
-		
-		System.out.println("몇 단을 출력할까요? : ");
-		Scanner scanner = new Scanner(System.in);
-		int number = scanner.nextInt();
-		
-		// 2~9 범위의 숫자를 올바르게 입력받았는가
-		if(number>=2 && number<=9) {
-			
+		for(int number =2; number<=9; number++) {
 			// 배열 생성
 			int[] result = new int[10];
 			
@@ -21,10 +14,6 @@ public class firstSource {
 			System.out.println("==== "+number+"단 ====");
 			for(int j=1; j<=9; j++)
 				System.out.println(number+ " X " + j + " = " + result[j]);
-			
-		} else {
-			System.out.println("2~9 범위의 숫자를 입력해주세요!!!");
 		}
-		
 	}
 }
