83726cb59ecfbf3e5228d172df74ea2e15724659
nablekim94 <nablekim94@gmail.com>
2021-11-22 17:40:48 +0900

    출력할 구구단의 숫자를 입력받아 출력하기 구현

--- MultiplicationTable/src/firstSource.java
+++ MultiplicationTable/src/firstSource.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
 
 public class firstSource {
 	public static void main(String[] args) {
-		for(int i=2; i<=3; i++) {
-			for(int j=1; j<=9; j++)
-				System.out.println(i+" X "+j+" = " + i*j);
-		}
+		
+		System.out.println("몇 단을 출력할까요? : ");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		
+		System.out.println("==== "+number+"단 ====");
+		for(int j=1; j<=9; j++)
+			System.out.println(number+" X "+j+" = " + number*j);
+		
 	}
 }
