a822cc85ef8096f14f0d252fa5d95f349efa0392
Nablekim94 <Nablekim94@gmail.com>
2021-12-12 23:26:18 +0900

    숫자를 입력받아 해당 월의 일수를 출력하기 with 함수

--- Calendar/src/nablekim/calendar/Calendar3.java
+++ Calendar/src/nablekim/calendar/Calendar3.java
@@ -3,13 +3,19 @@ package nablekim.calendar;
 import java.util.Scanner;
 
 public class Calendar3 {
+	
+	public static final int[] monArr = {31,28,31,30,31,30,31,31,30,31,30,31};
+	
+	public int getDaysOfMonth(int month) {
+		return monArr[month-1];
+	}
+	
 	public static void main(String[] args) {
-		
-		int[] monArr = {31,28,31,30,31,30,31,31,30,31,30,31};
-		
 		Scanner sc = new Scanner(System.in);		
 		int mon = sc.nextInt();
-		System.out.println(monArr[mon-1]);
+		
+		Calendar3 cal = new Calendar3();
+		System.out.println(cal.getDaysOfMonth(mon));
 
 		sc.close();
 	}
