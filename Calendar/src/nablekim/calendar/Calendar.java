f04bdd35f61bc28da9338584415794ba7adfbd30
Nablekim94 <Nablekim94@gmail.com>
2021-12-13 20:37:13 +0900

    입력받은 달의 시작 요일을 기준으로 달력 찍기 구현

--- Calendar/src/nablekim/calendar/Calendar.java
+++ Calendar/src/nablekim/calendar/Calendar.java
@@ -16,17 +16,34 @@ public class Calendar {
 		return MAX_DAYS[month - 1];
 	}
 	
-	public void printMonth(int year, int month) {
+	public int getWeekDay(String weekdayStr) {
+		if(weekdayStr.equals("SU")) return 0;
+		else if(weekdayStr.equals("MO")) return 1;
+		else if(weekdayStr.equals("TU")) return 2;
+		else if(weekdayStr.equals("WE")) return 3;
+		else if(weekdayStr.equals("TH")) return 4;
+		else if(weekdayStr.equals("FR")) return 5;
+		else if(weekdayStr.equals("SA")) return 6;
+		return 0;
+	}
+	
+	public void printMonth(int year, int month, String weekdayStr) {
 		int dayOfMonth = getDaysOfMonth(year, month);
+		
+		int weekday = getWeekDay(weekdayStr);
 		
 		System.out.printf("< %d년 %d월 >\n", year, month);
 		System.out.println("  일 월 화 수 목 금 토");
 		System.out.println("====================");
 		
+		for(int i=0; i<weekday; i++) {
+			System.out.printf("   ");
+		}
+		
 		for(int i=0; i<dayOfMonth; i++) {
 			System.out.printf("%3d", i+1);
-			if(i%7==6) System.out.print("\n");
+			if((i+weekday)%7==6) System.out.print("\n");
 		}
-		System.out.print("\n");
+		System.out.print("\n\n");
 	}
 }
--- Calendar/src/nablekim/calendar/Prompt.java
+++ Calendar/src/nablekim/calendar/Prompt.java
@@ -5,21 +5,29 @@ import java.util.Scanner;
 public class Prompt {
 	private static final String PROMPT_YEAR = "year> ";
 	private static final String PROMPT_MONTH = "month> ";
+	private static final String PROMPT_WEEKDAY = "weekday> ";
 	
 	public void runPrompt() {
 		Scanner sc = new Scanner(System.in);
 		Calendar cal = new Calendar();
 
 		while(true) {
+			System.out.println("종료하시려면 -1을 입력하세요.");
 			System.out.print(PROMPT_YEAR);
 			int year = sc.nextInt();
 			
+			if(year == -1) break;
+			
+			System.out.println("월 숫자(1~12)을 입력하세요.");
 			System.out.print(PROMPT_MONTH);
 			int month = sc.nextInt();
-			
-			if(month == -1) break;
 			if(month<1 || month>12) continue;
-			cal.printMonth(year, month);
+			
+			System.out.println("요일(SU, MO, TU, ... SA)을 입력하세요.");
+			System.out.print(PROMPT_WEEKDAY);
+			String weekday = sc.next();
+			
+			cal.printMonth(year, month, weekday);
 		}
 
 		sc.close();
