12dae088db86ae7835f3f98693c31d41389ccbaf
Nablekim94 <Nablekim94@gmail.com>
2021-12-14 11:47:14 +0900

    일정 관리 메뉴 및 달력 보기 메뉴 생성

--- Calendar/src/nablekim/calendar/Prompt.java
+++ Calendar/src/nablekim/calendar/Prompt.java
@@ -5,35 +5,60 @@ import java.util.Scanner;
 public class Prompt {
 	private static final String PROMPT_YEAR = "year> ";
 	private static final String PROMPT_MONTH = "month> ";
-	//private static final String PROMPT_DAY = "day> ";
+	private static final String PROMPT_MENU = "menu> ";
+	
+	public void printMenu() {
+		System.out.println("+-----------------------");
+		System.out.println("| 1. 일정 등록");
+		System.out.println("| 2. 일정 검색");
+		System.out.println("| 3. 달력 보기");
+		System.out.println("| h. 도움말");
+		System.out.println("| q. 종료");
+		System.out.println("+-----------------------");
+		System.out.println(PROMPT_MENU);
+	}
 	
 	public void runPrompt() {
+		
 		Scanner sc = new Scanner(System.in);
 		Calendar cal = new Calendar();
 
 		while(true) {
-			System.out.println("종료하시려면 -1을 입력하세요.");
-			System.out.print(PROMPT_YEAR);
-			int year = sc.nextInt();
-			
-			if(year == -1) break;
-			
-			System.out.println("월 숫자(1~12)을 입력하세요.");
-			System.out.print(PROMPT_MONTH);
-			int month = sc.nextInt();
-			if(month<1 || month>12) continue;
-			
-//			System.out.println("일 숫자(1~31)을 입력하세요.");
-//			System.out.print(PROMPT_DAY);
-//			int day = sc.nextInt();
-			//String weekday = sc.next();
-			
-			cal.printMonth(year, month);
+			printMenu();	// 메뉴 출력
+			String cmd = sc.next();
+			if(cmd.equals("1")) cmdRegister();
+			else if(cmd.equals("2")) cmdSearch();
+			else if(cmd.equals("3")) cmdCal(sc, cal);
+			else if(cmd.equals("h")) printMenu();
+			else if(cmd.equals("q")) break;
 		}
 
 		sc.close();
 	}
 	
+	private void cmdCal(Scanner sc, Calendar cal) {
+		System.out.println("년도(xxxx)를 입력하세요.");
+		System.out.print(PROMPT_YEAR);
+		int year = sc.nextInt();
+
+		System.out.println("월 숫자(1~12)을 입력하세요.");
+		System.out.print(PROMPT_MONTH);
+		int month = sc.nextInt();
+		if(month<1 || month>12) return;
+		
+		cal.printMonth(year, month);
+	}
+
+	private void cmdSearch() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void cmdRegister() {
+		// TODO Auto-generated method stub
+		
+	}
+
 	public static void main(String[] args) {
 		Prompt p = new Prompt();
 		p.runPrompt();
